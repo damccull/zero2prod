49b536ffae1ae0bf067bf957ff0d1dfd87426e68
damccull <401104+damccull@users.noreply.github.com>
2021-06-11 22:28:29 -0400

    Update subscriptions to enable saving users.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,34 @@
-use actix_web::web::{self, HttpResponse};
-use serde::Deserialize;
+use std::ops::Deref;
+use std::sync::Arc;
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+use actix_web::web::{self, HttpResponse};
+use chrono::Utc;
+use serde::Deserialize;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    db_pool: web::Data<Arc<PgPool>>,
+) -> Result<HttpResponse, HttpResponse> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_pool.get_ref().deref())
+    .await
+    .map_err(|e| {
+        eprintln!("Failed to execute query: {}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
+
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[derive(Clone, Debug, Deserialize)]
--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,21 @@
 use std::net::TcpListener;
+use std::sync::Arc;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 
 use crate::routes::*;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the connection in an Arc smart pointer so each instance of App can have its own reference.
+    let db_connection = Arc::new(db_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_connection.clone())
     })
     .listen(listener)?
     .run();
