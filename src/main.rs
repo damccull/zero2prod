1d3512f5674bcf08a6fecbd04ffcfdfbbcde5127
damccull <damccull+git@gmail.com>
2021-06-11 22:28:29 -0400

    Update subscriptions to enable saving users.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection, PgPool};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
@@ -9,11 +10,16 @@ async fn main() -> std::io::Result<()> {
     // Panic if config can't be read
     let configuration = get_configuration().expect("Failed to read configuration");
 
+    // Create a database connection for the web server.
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     // Create a `TcpListener` to pass to the web server.
     let listener = TcpListener::bind(format!(
         "127.0.0.1:{}",
         configuration.application.listen_port
     ))
     .expect("Failed to bind port.");
-    run(listener)?.await
+    run(listener, db_pool)?.await
 }
