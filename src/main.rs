67ccdaa5998ecff88326cb5908594b03fb118654
damccull <401104+damccull@users.noreply.github.com>
2021-06-28 13:01:33 -0400

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,16 @@ use std::net::TcpListener;
 use sqlx::{Connection, PgConnection, PgPool};
 use zero2prod::{configuration::get_configuration, startup::run};
 
+use env_logger::Env;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Initialize the logger
+    // `init` does call `set_logger`, so this is all we need to do
+    // Falling back to printing all logs at info-level or above if
+    // RUST_LOG env var is not set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if config can't be read
     let configuration = get_configuration().expect("Failed to read configuration");
 
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
+
 use sqlx::PgPool;
 
 use crate::routes::*;
@@ -12,6 +14,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
