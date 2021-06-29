c2a2b66cdf08d0d75ac8e40a5c4ba6dbcbd77572
damccull <401104+damccull@users.noreply.github.com>
2021-06-29 14:40:11 -0400

    Project builds in docker.

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,5 @@
+use std::convert::{TryFrom, TryInto};
+
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -8,6 +10,7 @@ pub struct Settings {
 
 #[derive(Debug, Deserialize)]
 pub struct ApplicationSettings {
+    pub listen_address: String,
     pub listen_port: u16,
 }
 
@@ -39,14 +42,58 @@ impl DatabaseSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Initialize the config reader
     let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory.");
+    let configuration_directory = base_path.join("configuration");
 
-    // Add config values from a file named 'configuration'.
-    // It looks for any top-level file with an extension that 'config' knows how to parse
-    // yaml, json, etc.
-    settings.merge(config::File::with_name("configuration"))?;
-    // Merge in a ci-specific config.
-    // settings.merge(config::File::with_name("configuration-ci"))?;
+    // Read the base config file
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    // Detect the running environment
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "development".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    // Layer on the environment-specific values
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
+
+    // Add settings from environment variables
+    // Prefix variables with 'APP_' and separate the path with '__'
+    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
 
     // Try to convert the configuration values into our settings type
     settings.try_into()
+}
+
+/// The possible environments for this app to run in
+pub enum Environment {
+    Development,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            &Environment::Development => "development",
+            &Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "development" => Ok(Self::Development),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either 'development' or 'production'.",
+                other
+            )),
+        }
+    }
 }
