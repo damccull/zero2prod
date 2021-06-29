57a1a22cd7e3f453b6e1bb0beff9ded4c0d836d8
damccull <401104+damccull@users.noreply.github.com>
2021-06-28 22:23:20 -0400

    Replace actix logger with tracing-actix-web

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,10 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::*;
 
@@ -14,7 +14,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
