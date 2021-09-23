44a4b5d66ffaeda62530a2a680636f39f94c98a7
damccull <401104+damccull@users.noreply.github.com>
2021-09-23 11:46:50 -0400

    Remove old files for fresh start.

--- migrations/20210611165518_create_subscriptions_table.sql
+++
@@ -1,8 +0,0 @@
--- Create subscriptions table
-CREATE TABLE subscriptions (
-    id uuid NOT NULL,
-    PRIMARY KEY (id),
-    email TEXT NOT NULL UNIQUE,
-    name TEXT NOT NULL,
-    subscribed_at timestamptz NOT NULL
-);
--- src/routes/health_check.rs
+++
@@ -1,5 +0,0 @@
-use actix_web::{HttpRequest, Responder, HttpResponse};
-
-pub async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
