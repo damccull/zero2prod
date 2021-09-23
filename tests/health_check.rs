27ecefbb9ed3a55f097dad63eef2ea7c51cfe109
damccull <401104+damccull@users.noreply.github.com>
2021-09-23 16:26:52 -0400

    Refactor to allow a random port in tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,23 +1,36 @@
+use std::net::TcpListener;
 
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
 
     // Use reqwest to test the endpoint over an http request
     let client = reqwest::Client::new();
 
     // Act
-    let response = client.get("http://127.0.0.1:8000/health_check").send().await.expect("Failed to execute request.");
+    let response = client
+        .get(format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-
 }
 
 // Somehow launch the app in the background
-    let server = zero2prod::run().expect("Failed to bind address");
-fn spawn_app() {
+fn spawn_app() -> String {
+    // Bind a random OS port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Get the random port from the listener
+    let port = listener.local_addr().unwrap().port();
+    // Use the listener for spinning up a server
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Execute the server in an Executor
     let _ = tokio::spawn(server);
+
+    // Return the port the the called
+    format!("http://127.0.0.1:{}", port)
 }
