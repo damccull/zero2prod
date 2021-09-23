52188019039488ca75aefe6de94fffc1cdcded5f
damccull <401104+damccull@users.noreply.github.com>
2021-09-23 16:10:29 -0400

    Add health check

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+
+#[actix_rt::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+
+    // Use reqwest to test the endpoint over an http request
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client.get("http://127.0.0.1:8000/health_check").send().await.expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+
+}
+
+// Somehow launch the app in the background
+async fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
