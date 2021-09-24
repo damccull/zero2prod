95b16491266203d1ba5899e97b68dc449a0ccd64
damccull <401104+damccull@users.noreply.github.com>
2021-09-23 21:02:08 -0400

    Add a configuration reader.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::net::TcpListener;
 
-use zero2prod::startup::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+    // Panic if config can't be read
+    let configuration = get_configuration().expect("Failed to read the configuration.");
+
+    // Port comes from the settings file
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
