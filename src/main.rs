11c41bfecbe8526de6d611aaf02d1752f9065f09
damccull <401104+damccull@users.noreply.github.com>
2021-09-24 09:07:58 -0400

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Set up logging to console, print all logs info and above by default
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if config can't be read
     let configuration = get_configuration().expect("Failed to read the configuration.");
 
