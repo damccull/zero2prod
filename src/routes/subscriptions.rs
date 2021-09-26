06e5f50b7ee3872c113abe1a69b6bf18037d5b51
damccull <damccull+git@gmail.com>
2021-09-26 10:50:38 -0400

    Remove extra UUID generator to unify all request-ids across a single request.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,7 +13,6 @@ pub struct FormData {
     name = "Adding a new subscriber",
     skip(form, pool),
     fields(
-        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
     )
