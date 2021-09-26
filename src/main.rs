17159fe579d12ee6413c0bab733bf94cacd30e25
damccull <401104+damccull@users.noreply.github.com>
2021-09-26 10:30:34 -0400

    Make the output target of logging changable.

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ use zero2prod::{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    ////
+    //
     // Set up logging using tracing, tracing-subscriber, and tracing-bunyan-formatter
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    ////
+    //
     // Panic if config can't be read
     let configuration = get_configuration().expect("Failed to read the configuration.");
 
