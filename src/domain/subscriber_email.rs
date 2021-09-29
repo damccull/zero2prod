bb0780bc26ddb8a0fc8bdfca93655c514cbc9653
damccull <401104+damccull@users.noreply.github.com>
2021-09-29 08:46:03 -0400

    Create a barebones SubscriberEmail without any validation.

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        //TODO: add validation!
+        Ok(Self(s))
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
