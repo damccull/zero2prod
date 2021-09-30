851fa385870311830a657ef4b4c985132fc5c5a3
damccull <401104+damccull@users.noreply.github.com>
2021-09-30 09:38:53 -0400

    Set up unit testing for email client.

--- src/email_client.rs
+++ src/email_client.rs
@@ -22,6 +22,42 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::{Fake, faker::{internet::en::SafeEmail, lorem::en::{Paragraph, Sentence}}};
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
+    use crate::domain::SubscriberEmail;
+
+    use super::EmailClient;
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        // Act
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+        
+        // Assert
     }
 }
