3c00215d6c9f0c3ccc00dbfa1ab2c194e37d7baf
damccull <401104+damccull@users.noreply.github.com>
2021-10-01 09:31:33 -0400

    Refactor to share application initialization between tests and main

--- src/startup.rs
+++ src/startup.rs
@@ -5,50 +5,63 @@ use actix_web::{
     web::{self, Data},
     App, HttpServer,
 };
-use sqlx::{postgres::PgPoolOptions, ConnectOptions, PgPool};
-use tracing::log::LevelFilter;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use tracing_actix_web::TracingLogger;
 
 use crate::{
-    configuration::Settings,
+    configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
 
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
-    // Create a database connection for the web server.
-    let db_connect_options = configuration
-        .database
-        .with_db()
-        .log_statements(LevelFilter::Trace)
-        .to_owned();
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
 
-    let connection_pool = PgPoolOptions::new()
+        // Build an `EmailClient` using the configuration
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+            timeout,
+        );
+
+        // Port comes from the settings file
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    // A more expressive name that makes it clear this function won't return until the application is stopped.
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(db_connect_options);
-
-    // Build an `EmailClient` using the configuration
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    // Port comes from the settings file
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-
-    run(listener, connection_pool, email_client)
+        .connect_lazy_with(configuration.with_db())
 }
 
 pub fn run(
