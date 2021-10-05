659e74d8a8667c8a71edffd471386245c5a6b536
damccull <401104+damccull@users.noreply.github.com>
2021-10-05 12:56:29 -0400

    Remove unnecessary transaction. Migrations already run in transactions.

--- migrations/20211002040251_make_status_not_null_in_subscriptions.sql
+++ migrations/20211002040251_make_status_not_null_in_subscriptions.sql
@@ -1,9 +1,6 @@
--- Wrap entire migration in a transaction to make sure it succeeds or fails as a unit
-BEGIN;
-    -- Backfill 'status' for historical entries
-    UPDATE subscriptions
-        SET status = 'confirmed'
-        WHERE status IS NULL;
-    -- Make 'status' mandatory
-    ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
-COMMIT;
+-- Backfill 'status' for historical entries
+UPDATE subscriptions
+    SET status = 'confirmed'
+    WHERE status IS NULL;
+-- Make 'status' mandatory
+ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
