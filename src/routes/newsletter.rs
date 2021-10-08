5b19fe1ffd3e7d50de40db314425f17423a2b874
damccull <401104+damccull@users.noreply.github.com>
2021-10-08 12:02:26 -0400

    Set up newsletters handler to accept body data from json

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,5 +1,18 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
+use serde::Deserialize;
 
-pub async fn publish_newsletter() -> HttpResponse {
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
 }
