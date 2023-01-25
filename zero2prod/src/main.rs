d41cb278ab877e8be515ef3ad036636cd43e65af
damccull <401104+damccull@users.noreply.github.com>
2023-01-24 20:48:42 -0500

    Set up tracing subscriber

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, World!");
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+#[tokio::main]
+async fn main() {
+    setup_tracing();
+}
+
+/// Sets up a tracing subscriber.
+fn setup_tracing() {
+    let fmt_layer = fmt::layer().compact().with_target(true);
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    let subscriber = tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer);
+
+    let _ = tracing::subscriber::set_global_default(subscriber)
+        .map_err(|_err| eprintln!("Unable to set global default subscriber"));
 }
