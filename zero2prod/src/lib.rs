fb02701c4dc3b4dc0d108ef3c7b31c8456f26867
damccull <401104+damccull@users.noreply.github.com>
2023-01-25 21:16:15 -0500

    Add a subscriptions endpoint

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -1,10 +1,18 @@
 use std::{future::Future, net::TcpListener};
 
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Router, Form,
+};
+use serde::Deserialize;
 
 pub fn run(listener: TcpListener) -> impl Future<Output = hyper::Result<()>> {
     // Create a router that will contain and match all routes for the application
-    let app = Router::new().route("/health_check", get(health_check));
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
 
     // Start the axum server and set up to use supplied listener
     axum::Server::from_tcp(listener)
@@ -15,4 +23,14 @@ pub fn run(listener: TcpListener) -> impl Future<Output = hyper::Result<()>> {
 /// Returns HTTP status code OK (200) to act as a health check
 pub async fn health_check() -> impl IntoResponse {
     StatusCode::OK
+}
+
+pub async fn subscribe(Form(form): Form<FormData>) -> impl IntoResponse {
+    StatusCode::OK
+}
+
+#[derive(Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
 }
