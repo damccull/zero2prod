b4369a8e186184a7648a49bb762b3d77598ed0c8
damccull <401104+damccull@users.noreply.github.com>
2023-03-08 19:11:22 -0600

    Add a method to get the connection string without the database name

--- zero2prod/src/configuration.rs
+++ zero2prod/src/configuration.rs
@@ -35,6 +35,13 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port, self.database_name
         )
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
 
 #[derive(Deserialize)]
