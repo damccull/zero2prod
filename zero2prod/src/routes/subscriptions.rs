5b5440c27ebad85dd45c64f2405e4af14d806634
damccull <401104+damccull@users.noreply.github.com>
2023-04-11 17:51:40 -0400

    Break out database insert to dedicated fn

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -3,14 +3,34 @@ use chrono::Utc;
 use serde::Deserialize;
 use sqlx::{types::Uuid, PgPool};
 
+#[tracing::instrument(name="Adding a new subscriber",skip(db,form),
+        fields(request_id=%Uuid::new_v4(),
+        subscriber_email=%form.email,
+        subscriber_name=%form.name
+    )
+)]
 pub async fn subscribe(State(db): State<PgPool>, Form(form): Form<FormData>) -> impl IntoResponse {
     tracing::info!(
         "Adding '{}' '{}' as a new subscriber.",
         form.email,
         form.name
     );
-    tracing::info!("Saving new subscriber details to the database.");
-    match sqlx::query!(
+    match insert_subscriber(&db, &form).await {
+        Ok(_) => {
+            tracing::info!("New subscriber details have been saved.");
+            StatusCode::OK
+        }
+        Err(e) => {
+            tracing::error!("failed to save subscriber details: {:?}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
+#[tracing::instrument(name="Saving new subscriber details in the database", skip(db,form))]
+async fn insert_subscriber(db: &PgPool, form: &FormData) -> Result<(),sqlx::Error> {
+    tracing::info!("saving sub deets");
+    sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
@@ -20,18 +40,14 @@ pub async fn subscribe(State(db): State<PgPool>, Form(form): Form<FormData>) -> 
         form.name,
         Utc::now()
     )
-    .execute(&db)
+    .execute(db)
     .await
-    {
-        Ok(_) => {
-            tracing::info!("New subscriber details have been saved.");
-            StatusCode::OK
-        }
-        Err(e) => {
-            tracing::error!("failed to execute query: {:?}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to exeucte query: {:?}", e);
+        e
+    })?;
+    Ok(())
+    
 }
 #[derive(Deserialize)]
 #[allow(dead_code)]
