d2097a94c8822eefb20b1d9654f0aa579e21188f
damccull <401104+damccull@users.noreply.github.com>
2023-04-12 13:15:33 -0400

    Instrument health check

--- zero2prod/src/routes/health_check.rs
+++ zero2prod/src/routes/health_check.rs
@@ -1,6 +1,7 @@
 use axum::{http::StatusCode, response::IntoResponse};
 
 /// Returns HTTP status code OK (200) to act as a health check
+#[tracing::instrument(name = "[Health Check]")]
 pub async fn health_check() -> impl IntoResponse {
     StatusCode::OK
 }
