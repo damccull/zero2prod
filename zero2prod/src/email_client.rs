6ce2eea18670c2138e528578bbc6bbcb14001e11
damccull <401104+damccull@users.noreply.github.com>
2023-04-18 12:26:15 -0400

    Create email client and allow multiple state sections to exist

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -1,10 +1,22 @@
+use reqwest::Client;
+
 use crate::domain::SubscriberEmail;
 
 pub struct EmailClient {
+    base_url: String,
+    http_client: Client,
     sender: SubscriberEmail,
 }
 
 impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+    
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
--- zero2prod/src/startup.rs
+++ zero2prod/src/startup.rs
@@ -1,24 +1,56 @@
-use std::{future::Future, net::TcpListener};
+use std::{future::Future, net::TcpListener, sync::Arc};
 
 use axum::{
+    extract::FromRef,
     routing::{get, post},
     Router,
 };
 use sqlx::PgPool;
 
-use crate::routes::{health_check, subscribe};
 use crate::telemetry::RouterExt;
+use crate::{
+    email_client::EmailClient,
+    routes::{health_check, subscribe},
+};
 
-pub fn run(listener: TcpListener, connection: PgPool) -> impl Future<Output = hyper::Result<()>> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> impl Future<Output = hyper::Result<()>> {
+    // Build app state
+    let app_state = AppState {
+        db_pool,
+        email_client: Arc::new(email_client),
+    };
+
     // Create a router that will contain and match all routes for the application
     let app = Router::new()
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe))
         .add_axum_tracing_layer()
-        .with_state(connection);
+        .with_state(app_state);
 
     // Start the axum server and set up to use supplied listener
     axum::Server::from_tcp(listener)
         .expect("failed to create server from listener")
         .serve(app.into_make_service())
+}
+
+#[derive(Clone)]
+struct AppState {
+    db_pool: PgPool,
+    email_client: Arc<EmailClient>,
+}
+
+impl FromRef<AppState> for PgPool {
+    fn from_ref(app_state: &AppState) -> Self {
+        app_state.db_pool.clone()
+    }
+}
+
+impl FromRef<AppState> for Arc<EmailClient> {
+    fn from_ref(app_state: &AppState) -> Self {
+        app_state.email_client.clone()
+    }
 }
