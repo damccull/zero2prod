1dc1e259086b0084e5c6cf2eb2a40fa5786273a7
damccull <401104+damccull@users.noreply.github.com>
2023-04-18 15:05:39 -0400

    Add test to ensure 200 on successful email

--- zero2prod/src/email_client.rs
+++ zero2prod/src/email_client.rs
@@ -69,6 +69,7 @@ struct SendEmailRequest<'a> {
 
 #[cfg(test)]
 mod tests {
+    use claims::assert_ok;
     use fake::{
         faker::{
             internet::en::SafeEmail,
@@ -78,7 +79,7 @@ mod tests {
     };
     use secrecy::Secret;
     use wiremock::{
-        matchers::{header, header_exists, method, path},
+        matchers::{any, header, header_exists, method, path},
         Mock, MockServer, Request, ResponseTemplate,
     };
 
@@ -129,5 +130,31 @@ mod tests {
             .await;
 
         // Assertions will happen when the mock server goes out of scope
+    }
+
+    #[tokio::test]
+    async fn send_email_succeeds_if_the_server_returns_200() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Act
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        assert_ok!(outcome);
     }
 }
