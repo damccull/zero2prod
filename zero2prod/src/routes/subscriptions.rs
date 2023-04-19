eade1d560923bc38db96427768b87fc8ae52119e
damccull <401104+damccull@users.noreply.github.com>
2023-04-18 23:08:42 -0400

    Add 'confirmed' to each new subscription automatically

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -47,8 +47,8 @@ pub async fn subscribe(State(db): State<PgPool>, Form(form): Form<FormData>) -> 
 async fn insert_subscriber(db: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+    VALUES ($1, $2, $3, $4, 'confirmed')
     "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
