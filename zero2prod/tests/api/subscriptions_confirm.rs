78109ea76ffbe707d38d4e28e38d83895b0c369b
damccull <401104+damccull@users.noreply.github.com>
2023-04-20 15:51:13 -0400

    Add test to check subscriptions confirmations

---
+++ zero2prod/tests/api/subscriptions_confirm.rs
@@ -0,0 +1,15 @@
+use crate::helpers::spawn_app;
+
+#[tokio::test]
+async fn confirmations_without_token_are_rejected_with_a_400() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act
+    let response = reqwest::get(&format!("{}/subscriptions/confirm", app.address))
+        .await
+        .unwrap();
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 400);
+}
