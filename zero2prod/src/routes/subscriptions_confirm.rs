882fd7bc432f91ef67a2d68e3584899a4aabfd1a
damccull <401104+damccull@users.noreply.github.com>
2023-04-20 19:22:08 -0400

    Fix warnings.

--- zero2prod/src/routes/subscriptions_confirm.rs
+++ zero2prod/src/routes/subscriptions_confirm.rs
@@ -2,12 +2,12 @@ use axum::{extract::Query, response::IntoResponse};
 use http::StatusCode;
 use serde::Deserialize;
 
-#[tracing::instrument(name = "Confirm a pending subscription", skip(parameters))]
-pub async fn confirm(parameters: Query<ConfirmParameters>) -> impl IntoResponse {
+#[tracing::instrument(name = "Confirm a pending subscription", skip(_parameters))]
+pub async fn confirm(_parameters: Query<ConfirmParameters>) -> impl IntoResponse {
     StatusCode::OK
 }
 
 #[derive(Debug, Deserialize)]
 pub struct ConfirmParameters {
-    subscription_token: String,
+    _subscription_token: String,
 }
