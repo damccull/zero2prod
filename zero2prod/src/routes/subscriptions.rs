53c8d262982d7e914df46ae9cf8e91280c44d750
damccull <401104+damccull@users.noreply.github.com>
2023-04-20 14:03:28 -0400

    Change default status of new subscriber

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -83,7 +83,7 @@ async fn insert_subscriber(db: &PgPool, new_subscriber: &NewSubscriber) -> Resul
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-    VALUES ($1, $2, $3, $4, 'confirmed')
+    VALUES ($1, $2, $3, $4, 'pending_confirmation')
     "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
