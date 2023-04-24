f82bbc38c91394f256d69d99f758eb89902798fc
damccull <401104+damccull@users.noreply.github.com>
2023-04-24 15:33:33 -0400

    Implement custom PublishError for newsletters endpoint

--- zero2prod/src/routes/newsletters.rs
+++ zero2prod/src/routes/newsletters.rs
@@ -1,8 +1,15 @@
-use axum::{response::IntoResponse, Json};
+use axum::{extract::State, response::IntoResponse, Json};
 use http::StatusCode;
+use sqlx::PgPool;
 
-pub async fn publish_newsletter(Json(body_data): Json<BodyData>) -> impl IntoResponse {
-    StatusCode::OK
+use crate::error_chain_fmt;
+
+pub async fn publish_newsletter(
+    State(db_pool): State<PgPool>,
+    Json(body_data): Json<BodyData>,
+) -> Result<impl IntoResponse, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&db_pool).await?;
+    Ok(StatusCode::OK)
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(db_pool))]
@@ -36,4 +43,25 @@ pub struct BodyData {
 pub struct Content {
     html: String,
     text: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl IntoResponse for PublishError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+        .into_response()
+    }
 }
