86c02b439f7f8062c7b6d375773b5d20f3e72a41
damccull <401104+damccull@users.noreply.github.com>
2023-04-24 15:29:39 -0400

    Add fn to get confirmed subscribers from db

--- zero2prod/src/routes/newsletters.rs
+++ zero2prod/src/routes/newsletters.rs
@@ -5,6 +5,27 @@ pub async fn publish_newsletter(Json(body_data): Json<BodyData>) -> impl IntoRes
     StatusCode::OK
 }
 
+#[tracing::instrument(name = "Get confirmed subscribers", skip(db_pool))]
+async fn get_confirmed_subscribers(
+    db_pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(db_pool)
+    .await?;
+    Ok(rows)
+}
+
+struct ConfirmedSubscriber {
+    email: String,
+}
+
 #[derive(Debug, serde::Deserialize)]
 pub struct BodyData {
     title: String,
