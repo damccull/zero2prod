8ec42ad0f413af1a9dd9bd88780a265c92a17bd8
damccull <401104+damccull@users.noreply.github.com>
2023-04-24 21:04:33 -0400

    Add test for basic auth

--- zero2prod/tests/api/newsletters.rs
+++ zero2prod/tests/api/newsletters.rs
@@ -150,3 +150,29 @@ async fn newsletters_returns_422_for_invalid_data() {
         );
     }
 }
+
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    // Arrange
+    let app = spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title":"Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body ad HTML</p>"
+                    }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    )
+}
