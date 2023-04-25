e3b34560b2435c51eb8052c96b8c29f31bac0f17
damccull <401104+damccull@users.noreply.github.com>
2023-04-25 14:21:53 -0400

    Refactor to use spawn_blocking_with_tracing

--- zero2prod/src/routes/newsletters.rs
+++ zero2prod/src/routes/newsletters.rs
@@ -10,7 +10,9 @@ use http::{HeaderMap, StatusCode};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
-use crate::{domain::SubscriberEmail, email_client::EmailClient};
+use crate::{
+    domain::SubscriberEmail, email_client::EmailClient, telemetry::spawn_blocking_with_tracing,
+};
 
 use newsletter_errors::*;
 use newsletter_types::*;
@@ -70,9 +72,8 @@ async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
 
-    let current_span = tracing::Span::current();
-    tokio::task::spawn_blocking(move || {
-        current_span.in_scope(|| verify_password_hash(expected_password_hash, credentials.password))
+    spawn_blocking_with_tracing(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task.")
