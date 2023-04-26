3aabfc0b35a7f1fe6bbbf19153a1b3a576c4429e
damccull <401104+damccull@users.noreply.github.com>
2023-04-26 13:57:02 -0400

    Extract form data and initial error code

--- zero2prod/src/routes/login/post.rs
+++ zero2prod/src/routes/login/post.rs
@@ -1,6 +1,40 @@
-use axum::response::{IntoResponse, Redirect};
+use axum::{
+    response::{IntoResponse, Redirect},
+    Form,
+};
+use axum_macros::debug_handler;
+use http::StatusCode;
+use secrecy::Secret;
+use serde::Deserialize;
 
-#[tracing::instrument(name = "Login posted")]
-pub async fn login() -> impl IntoResponse {
-    Redirect::to("/").into_response()
+use crate::error_chain_fmt;
+
+#[debug_handler]
+#[tracing::instrument(
+    name = "Login posted"
+    skip(form_data)
+)]
+pub async fn login(Form(form_data): Form<FormData>) -> Result<impl IntoResponse, LoginErrorTemp> {
+    Ok(Redirect::to("/").into_response())
+}
+
+#[derive(Deserialize)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
+
+#[derive(thiserror::Error)]
+#[error(transparent)]
+pub struct LoginErrorTemp(#[from] anyhow::Error);
+impl IntoResponse for LoginErrorTemp {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "It broke").into_response()
+    }
+}
+
+impl std::fmt::Debug for LoginErrorTemp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
 }
