f2b39870d550b89c69218dea37b27250ba56d321
damccull <401104+damccull@users.noreply.github.com>
2023-04-26 14:42:40 -0400

    Refactor login to accept or reject based on credentials

--- zero2prod/src/routes/login/post.rs
+++ zero2prod/src/routes/login/post.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::State,
     response::{IntoResponse, Redirect},
     Form,
 };
@@ -6,16 +7,40 @@ use axum_macros::debug_handler;
 use http::StatusCode;
 use secrecy::Secret;
 use serde::Deserialize;
+use sqlx::PgPool;
 
-use crate::error_chain_fmt;
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    error_chain_fmt,
+};
 
 #[debug_handler]
 #[tracing::instrument(
     name = "Login posted"
-    skip(form_data)
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
-pub async fn login(Form(form_data): Form<FormData>) -> Result<impl IntoResponse, LoginErrorTemp> {
-    Ok(Redirect::to("/").into_response())
+pub async fn login(
+    State(pool): State<PgPool>,
+    Form(form): Form<FormData>,
+) -> Result<impl IntoResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.username,
+        password: form.password,
+    };
+
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+    Ok(Redirect::to("/"))
 }
 
 #[derive(Deserialize)]
@@ -25,15 +50,23 @@ pub struct FormData {
 }
 
 #[derive(thiserror::Error)]
-#[error(transparent)]
-pub struct LoginErrorTemp(#[from] anyhow::Error);
-impl IntoResponse for LoginErrorTemp {
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+impl IntoResponse for LoginError {
     fn into_response(self) -> axum::response::Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, "It broke").into_response()
+        tracing::error!("{:?}", self);
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED.into_response(),
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+        }
     }
 }
 
-impl std::fmt::Debug for LoginErrorTemp {
+impl std::fmt::Debug for LoginError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
     }
