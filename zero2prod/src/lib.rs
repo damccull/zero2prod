77c4a756d92d7dc8a270433c3b69c071de2d65e9
damccull <401104+damccull@users.noreply.github.com>
2023-05-10 23:43:15 -0400

    Create a custom typed extractor for session.

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -4,6 +4,7 @@ pub mod domain;
 pub mod email_client;
 pub mod error;
 pub mod routes;
+pub mod session_state;
 pub mod startup;
 pub mod telemetry;
 
---
+++ zero2prod/src/session_state.rs
@@ -0,0 +1,43 @@
+use axum::{async_trait, extract::FromRequestParts};
+use axum_session::{DatabasePool, Session};
+use uuid::Uuid;
+
+pub struct TypedSession<T>(Session<T>)
+where
+    T: DatabasePool + Clone + std::fmt::Debug + Sync + Send + 'static;
+impl<T> TypedSession<T>
+where
+    T: DatabasePool + Clone + std::fmt::Debug + Sync + Send + 'static,
+{
+    const USER_ID_KEY: &'static str = "user_id";
+
+    pub fn renew(&self) {
+        self.0.renew();
+    }
+
+    pub fn insert_user_id(&self, user_id: Uuid) {
+        self.0.set(Self::USER_ID_KEY, user_id)
+    }
+
+    pub fn get_user_id(&self) -> Option<Uuid> {
+        self.0.get(Self::USER_ID_KEY)
+    }
+}
+
+#[async_trait]
+impl<T, S> FromRequestParts<S> for TypedSession<T>
+where
+    T: DatabasePool + Clone + std::fmt::Debug + Sync + Send + 'static,
+    S: Send + Sync,
+{
+    type Rejection = <Session<T> as FromRequestParts<S>>::Rejection;
+
+    async fn from_request_parts(
+        parts: &mut http::request::Parts,
+        state: &S,
+    ) -> Result<Self, Self::Rejection> {
+        let session: Session<T> = Session::<T>::from_request_parts(parts, state).await?;
+
+        Ok(TypedSession(session))
+    }
+}
