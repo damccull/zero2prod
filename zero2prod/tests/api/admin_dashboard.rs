83ed8ded3286d2925cdfefcfca22a516edd6d1b1
damccull <401104+damccull@users.noreply.github.com>
2023-05-11 00:18:08 -0400

    Add a test to reject unauthenticated users to admin dashboard.

---
+++ zero2prod/tests/api/admin_dashboard.rs
@@ -0,0 +1,13 @@
+use crate::{helpers::spawn_app, login::assert_is_redirect_to};
+
+#[tokio::test]
+async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act
+    let response = app.get_admin_dashboard().await;
+
+    // Assert
+    assert_is_redirect_to(&response, "/login");
+}
