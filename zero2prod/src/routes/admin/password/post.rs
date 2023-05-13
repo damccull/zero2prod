d8021ba4e2e5bd8cb7e6eff44a9ebab27d1e5dd3
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 14:35:37 -0400

    Flesh out the admin password change post endpoint.

--- zero2prod/src/routes/admin/password/post.rs
+++ zero2prod/src/routes/admin/password/post.rs
@@ -1,1 +1,21 @@
-pub async fn change_password() {}
+use axum::{response::IntoResponse, Form};
+use http::StatusCode;
+use secrecy::Secret;
+use serde::Deserialize;
+
+use crate::error::ResponseInternalServerError;
+
+#[tracing::instrument(name = "Change password", skip(form))]
+pub async fn change_password(
+    Form(form): Form<FormData>,
+) -> Result<impl IntoResponse, ResponseInternalServerError<anyhow::Error>> {
+    todo!();
+    Ok(StatusCode::OK)
+}
+
+#[derive(Deserialize)]
+pub struct FormData {
+    current_password: Secret<String>,
+    new_password: Secret<String>,
+    new_password_check: Secret<String>,
+}
