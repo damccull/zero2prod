39e3166e84fb69a6c58b06f4ece5a8c1cb4017a4
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 15:24:14 -0400

    Send flash message on unmatched passwords.

--- zero2prod/src/routes/admin/password/post.rs
+++ zero2prod/src/routes/admin/password/post.rs
@@ -2,22 +2,28 @@ use axum::{
     response::{IntoResponse, Redirect},
     Form,
 };
+use axum_flash::Flash;
 use axum_session::SessionRedisPool;
-use http::StatusCode;
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 
 use crate::{error::ResponseInternalServerError, session_state::TypedSession};
 
 #[tracing::instrument(name = "Change password", skip(session, form))]
 pub async fn change_password(
+    flash: Flash,
     session: TypedSession<SessionRedisPool>,
     Form(form): Form<FormData>,
 ) -> Result<impl IntoResponse, ResponseInternalServerError<anyhow::Error>> {
     if session.get_user_id().is_none() {
         return Ok(Redirect::to("/login").into_response());
     }
-    Ok(StatusCode::OK.into_response())
+    if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
+        let flash =
+            flash.error("You entered two different new passwords - the field values must match.");
+        return Ok((flash, Redirect::to("/admin/password")).into_response());
+    }
+    todo!()
 }
 
 #[derive(Deserialize)]
