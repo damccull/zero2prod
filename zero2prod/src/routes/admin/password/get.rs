96f0136d51b30a6f811bb63ccdf45b593094afcc
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 15:23:56 -0400

    Display flash messages.

--- zero2prod/src/routes/admin/password/get.rs
+++ zero2prod/src/routes/admin/password/get.rs
@@ -1,24 +1,39 @@
 use axum::response::{IntoResponse, Redirect};
 use axum_extra::response::Html;
+use axum_flash::{IncomingFlashes, Level};
 use axum_session::SessionRedisPool;
 use http::StatusCode;
+use std::fmt::Write;
 
 use crate::{error::ResponseInternalServerError, session_state::TypedSession};
 
 #[tracing::instrument("Change password form" skip(session))]
 pub async fn change_password_form(
+    flashes: IncomingFlashes,
     session: TypedSession<SessionRedisPool>,
 ) -> Result<impl IntoResponse, ResponseInternalServerError<anyhow::Error>> {
     if session.get_user_id().is_none() {
         return Ok(Redirect::to("/login").into_response());
     }
-    let body = r#"<!DOCTYPE html>
+
+    let mut msg_html = String::new();
+    for (level, text) in flashes.iter().filter(|m| m.0 == Level::Error) {
+        writeln!(
+            msg_html,
+            "<p><strong>{:?}</strong>: <i>{}</i></p>\n",
+            level, text
+        )
+        .unwrap();
+    }
+    let body = format!(
+        r#"<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>Change Password</title>
 </head>
 <body>
+    {msg_html}
     <form action="/admin/password" method="post">
         <label>Current password
             <input type="password" placeholder="Enter current password" name="current_password">
@@ -35,7 +50,8 @@ pub async fn change_password_form(
         <button type="submit">Change password</button>
     </form>
 </body>
-</html>"#;
+</html>"#
+    );
 
     Ok(Html((StatusCode::OK, body)).into_response())
 }
