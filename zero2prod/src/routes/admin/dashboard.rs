4d094ee4f1365ee3943fd066e1be845fa7b5dc3b
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 20:46:28 -0400

    Make fn pub.

--- zero2prod/src/routes/admin/dashboard.rs
+++ zero2prod/src/routes/admin/dashboard.rs
@@ -48,7 +48,7 @@ pub async fn admin_dashboard(
 }
 
 #[tracing::instrument(name = "Get username", skip(pool))]
-async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
+pub async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
     let row = sqlx::query!(
         r#"
         SELECT username
