d5f879e7a672811e9be60c803191439508c94c69
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 22:48:17 -0400

    Insert the session's user id into the request extensions

--- zero2prod/src/authentication.rs
+++ zero2prod/src/authentication.rs
@@ -1,5 +1,5 @@
 mod middleware;
 mod password;
 
-pub use middleware::reject_anonymous_users;
+pub use middleware::{reject_anonymous_users, UserId};
 pub use password::{change_password, validate_credentials, AuthError, Credentials};
--- zero2prod/src/authentication/middleware.rs
+++ zero2prod/src/authentication/middleware.rs
@@ -1,22 +1,45 @@
+use std::ops::Deref;
+
 use axum::{
     middleware::Next,
     response::{IntoResponse, Redirect, Response},
 };
 use axum_session::SessionRedisPool;
 use http::Request;
+use uuid::Uuid;
 
 use crate::session_state::TypedSession;
 
 pub async fn reject_anonymous_users<B>(
     session: TypedSession<SessionRedisPool>,
-    request: Request<B>,
+    mut request: Request<B>,
     next: Next<B>,
 ) -> Result<Response, impl IntoResponse> {
     match session.get_user_id() {
-        Some(_) => Ok(next.run(request).await),
+        Some(uid) => {
+            request.extensions_mut().insert(UserId(uid));
+            Ok(next.run(request).await)
+        }
         None => {
             tracing::error!("User has not logged in.");
             Err(Redirect::to("/login"))
         }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct UserId(Uuid);
+
+impl std::fmt::Display for UserId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Deref for UserId {
+    type Target = Uuid;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
