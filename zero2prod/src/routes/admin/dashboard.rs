f49e8d43e0f5a8c464ccc052ba484d3cbdf23d41
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 21:15:07 -0400

    Add a logout button.

--- zero2prod/src/routes/admin/dashboard.rs
+++ zero2prod/src/routes/admin/dashboard.rs
@@ -38,6 +38,11 @@ pub async fn admin_dashboard(
     <p>Available actions:</p>
     <ol>
         <li><a href="/admin/password">Change password</a></li>
+        <li>
+            <form name="logoutForm" action="/admin/logout" method="post">
+                <input type="submit" value="Logout">
+            </form>
+        </li>
     </ol>
 </body>
 </html>
