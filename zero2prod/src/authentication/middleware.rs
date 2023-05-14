397e84651912df119fd2ccf87a1cf946e22cbf94
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 22:34:26 -0400

    Add simple middleware fn to reject anonymous users.

--- zero2prod/src/authentication/middleware.rs
+++ zero2prod/src/authentication/middleware.rs
@@ -1,1 +1,22 @@
-pub async fn reject_anonymous_users() {}
+use axum::{
+    middleware::Next,
+    response::{IntoResponse, Redirect, Response},
+};
+use axum_session::SessionRedisPool;
+use http::Request;
+
+use crate::session_state::TypedSession;
+
+pub async fn reject_anonymous_users<B>(
+    session: TypedSession<SessionRedisPool>,
+    request: Request<B>,
+    next: Next<B>,
+) -> Result<Response, impl IntoResponse> {
+    match session.get_user_id() {
+        Some(_) => Ok(next.run(request).await),
+        None => {
+            tracing::error!("User has not logged in.");
+            Err(Redirect::to("/login"))
+        }
+    }
+}
