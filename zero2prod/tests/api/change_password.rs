e76515babb1ba4db81125e32e10cf6ff97a905d3
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 20:47:01 -0400

    Add a test to ensure current password must be valid

--- zero2prod/tests/api/change_password.rs
+++ zero2prod/tests/api/change_password.rs
@@ -63,3 +63,36 @@ async fn new_password_fields_must_match() {
     assert!(html_page
         .contains("You entered two different new passwords - the field values must match."))
 }
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    // Act - Part 1 - Login
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - Try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &wrong_password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+
+    // Assert - Part 2
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - Follow the redirect
+    let html_page = app.get_change_password_html().await;
+
+    // Assert - Part 3
+    assert!(html_page.contains("The current password is incorrect."));
+}
