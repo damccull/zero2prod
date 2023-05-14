f3e2f8484716e5f3549fc95830ba7b86f518d5c5
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 21:26:43 -0400

    Add logout capability.

--- zero2prod/src/routes/admin/logout.rs
+++ zero2prod/src/routes/admin/logout.rs
@@ -1,1 +1,18 @@
-pub async fn log_out() {}
+use axum::response::{IntoResponse, Redirect};
+use axum_flash::Flash;
+use axum_session::SessionRedisPool;
+
+use crate::{error::ResponseInternalServerError, session_state::TypedSession};
+
+pub async fn log_out(
+    flash: Flash,
+    session: TypedSession<SessionRedisPool>,
+) -> Result<impl IntoResponse, ResponseInternalServerError<anyhow::Error>> {
+    if session.get_user_id().is_none() {
+        Ok(Redirect::to("/login").into_response())
+    } else {
+        session.log_out();
+        let flash = flash.info("You have successfully logged out.");
+        Ok((flash, Redirect::to("/login")).into_response())
+    }
+}
