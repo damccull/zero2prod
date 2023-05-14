7c4ed2775e06d95560e7bb2af226706be7e8b223
damccull <401104+damccull@users.noreply.github.com>
2023-05-13 21:40:11 -0400

    Add test to ensure changing password works.

--- zero2prod/tests/api/change_password.rs
+++ zero2prod/tests/api/change_password.rs
@@ -134,3 +134,50 @@ async fn new_password_must_be_correct_length() {
         assert!(html_page.contains("The new password should be between 8 and 128 characters long."));
     }
 }
+
+#[tokio::test]
+async fn changing_password_works() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+
+    // Act - Part 1 - Login
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    });
+
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    // Act - Part 2 - Change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - Follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains("Your password has been changed."));
+
+    // Act - Part 4 - Logout
+    let response = app.post_logout().await;
+    assert_is_redirect_to(&response, "/login");
+
+    // Act - Part 5 - Follow the redirect
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains("You have successfully logged out."));
+
+    // Act - Part 6 - Login using the new password
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &new_password,
+    });
+
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+}
