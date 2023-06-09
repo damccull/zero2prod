07c8e6d8aab52ae2c64c9e7a2424bbeb604c5d03
damccull <401104+damccull@users.noreply.github.com>
2023-06-09 14:41:27 -0400

    Re-flatten the newsletter body data struct

--- zero2prod/src/routes/admin/newsletters.rs
+++ zero2prod/src/routes/admin/newsletters.rs
@@ -4,14 +4,9 @@ mod post;
 pub use get::newsletters_publish_form;
 pub use post::publish_newsletter;
 
-#[derive(Debug, serde::Deserialize)]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct BodyData {
     pub title: String,
-    pub content: Content,
-}
-
-#[derive(Debug, serde::Deserialize)]
-pub struct Content {
-    pub html: String,
-    pub text: String,
+    pub html_content: String,
+    pub text_content: String,
 }
