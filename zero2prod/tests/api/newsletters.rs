380e7ed3c71349170e9d34f79e43b25e3e2e6bca
damccull <401104+damccull@users.noreply.github.com>
2023-06-08 23:14:46 -0400

    Remove another unneeded test

--- zero2prod/tests/api/newsletters.rs
+++ zero2prod/tests/api/newsletters.rs
@@ -177,35 +177,3 @@ async fn requests_missing_authorization_are_rejected() {
         response.headers()["WWW-Authenticate"]
     )
 }
-
-#[tokio::test]
-async fn non_existent_user_is_rejected() {
-    // Arrange
-    let app = spawn_app().await;
-
-    // Random credentials
-    let username = Uuid::new_v4().to_string();
-    let password = Uuid::new_v4().to_string();
-
-    // Act
-    let response = reqwest::Client::new()
-        .post(&format!("{}/admin/newsletters", &app.address))
-        .basic_auth(username, Some(password))
-        .json(&serde_json::json!({
-            "title":"Newsletter title",
-            "content":{
-                "text":"Newsletter body as plain text",
-                "html":"<p>Newsletter body as HTML</p>",
-            }
-        }))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    // Assert
-    assert_eq!(401, response.status().as_u16());
-    assert_eq!(
-        r#"Basic realm="publish""#,
-        response.headers()["WWW-Authenticate"]
-    );
-}
