9ced1c35ac91c0976d93555466894b34f0d524b1
damccull <401104+damccull@users.noreply.github.com>
2023-06-24 12:13:43 -0400

    Create fn to save reponse items in cache

--- zero2prod/src/idempotency.rs
+++ zero2prod/src/idempotency.rs
@@ -3,3 +3,4 @@ mod persistence;
 
 pub use key::IdempotencyKey;
 pub use persistence::get_saved_response;
+pub use persistence::save_response;
--- zero2prod/src/idempotency/persistence.rs
+++ zero2prod/src/idempotency/persistence.rs
@@ -1,6 +1,7 @@
 use axum::response::{IntoResponse, Response};
 use http::{HeaderMap, HeaderName, HeaderValue, StatusCode};
-use sqlx::PgPool;
+use hyper::body::to_bytes;
+use sqlx::{postgres::PgHasArrayType, PgPool};
 use uuid::Uuid;
 
 use super::IdempotencyKey;
@@ -43,9 +44,58 @@ pub async fn get_saved_response(
     }
 }
 
+pub async fn save_response(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: Response,
+) -> Result<Response, anyhow::Error> {
+    let (response_head, body) = http_response.into_parts();
+    let body = to_bytes(body).await.map_err(|e| anyhow::anyhow!("{}", e))?;
+    let status_code = response_head.status.as_u16() as i16;
+    let headers = {
+        let mut h = Vec::with_capacity(response_head.headers.len());
+        for (name, value) in response_head.headers.iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            h.push(HeaderPairRecord { name, value });
+        }
+        h
+    };
+
+    sqlx::query_unchecked!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            response_status_code,
+            response_headers,
+            response_body,
+            created_at
+        )
+        VALUES ($1, $2, $3, $4, $5, now())
+        "#,
+        user_id,
+        idempotency_key.as_ref(),
+        status_code,
+        headers,
+        body.as_ref()
+    )
+    .execute(pool)
+    .await?;
+    let http_response = (response_head, body).into_response();
+    Ok(http_response)
+}
+
 #[derive(Debug, sqlx::Type)]
 #[sqlx(type_name = "header_pair")]
 struct HeaderPairRecord {
     name: String,
     value: Vec<u8>,
+}
+
+impl PgHasArrayType for HeaderPairRecord {
+    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("_header_pair")
+    }
 }
